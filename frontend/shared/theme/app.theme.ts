9807eb62b8325e468f62b8a3814ba860eae838ac
LeonardTarigan <leontrg32@gmail.com>
2026-06-13 13:37:04 +0700

    chore: override mantine gray

--- frontend/shared/theme/app.theme.ts
+++ frontend/shared/theme/app.theme.ts
@@ -15,6 +15,18 @@ export const appTheme = createTheme({
             '#287d7b',
             '#206f6d',
             '#155c5a',
+        ],
+        gray: [
+            '#fafafa',
+            '#f4f4f5',
+            '#e4e4e7',
+            '#d4d4d8',
+            '#a1a1aa',
+            '#71717a',
+            '#52525b',
+            '#3f3f46',
+            '#27272a',
+            '#18181b',
         ]
     },
     primaryColor: 'primary',
